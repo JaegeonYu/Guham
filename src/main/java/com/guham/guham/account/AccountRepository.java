4b4bb3c011dbb479fa8fb079a0cde05eef5a87ca
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-05-01 15:33:08 +0900

    Feat: AccountReposiotry 생성

---
+++ src/main/java/com/guham/guham/account/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.guham.guham.account;
+
+import com.guham.guham.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
