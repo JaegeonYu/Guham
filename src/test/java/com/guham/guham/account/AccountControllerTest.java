6f55f3049241f5cacd1edd0a48666a3a223c986d
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-05-01 14:04:16 +0900

    Feat: AccountController SignUpForm 구현, 테스트

---
+++ src/test/java/com/guham/guham/account/AccountControllerTest.java
@@ -0,0 +1,33 @@
+package com.guham.guham.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    private final MockMvc mockMvc;
+
+    @Autowired
+    public AccountControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    @DisplayName("회원 가입 화면 테스트")
+    public void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(model().attributeExists("signUpForm"));
+    }
+}
