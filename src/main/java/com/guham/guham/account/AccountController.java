81a3b265b9f2abc8c15f84b90db5af330fabe2d0
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-05-01 16:33:41 +0900

    Refactor: SignUp Process Controller -> Service

--- src/main/java/com/guham/guham/account/AccountController.java
+++ src/main/java/com/guham/guham/account/AccountController.java
@@ -21,8 +21,8 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AccountController {
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
+
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -41,26 +41,10 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword())
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .build();
-
-        Account newAccount = accountRepository.save(account);
-
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("구함, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
-                "&email=" + newAccount.getEmail());
-
-
+        accountService.signUp(signUpForm);
         // TODO 회원 가입 처리
         return "redirect:/";
     }
+
+
 }
