0e51ae0bef87972ac935175f5d8382757b368294
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-05-07 16:31:34 +0900

    Feat: NicknameForm 구현

--- src/main/java/com/guham/guham/settings/validator/PasswordFormValidator.java
+++ src/main/java/com/guham/guham/settings/validator/PasswordFormValidator.java
@@ -1,5 +1,6 @@
 package com.guham.guham.settings;
 
+import com.guham.guham.settings.form.PasswordForm;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
