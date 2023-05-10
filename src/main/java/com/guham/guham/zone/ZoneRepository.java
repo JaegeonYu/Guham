315a19fbe8b6527bb1c7b39533fe678e3eff5719
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-05-10 13:25:24 +0900

    Feat: Zone Entity 추가, Zone 정보 초기화 설정

---
+++ src/main/java/com/guham/guham/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.guham.guham.zone;
+
+import com.guham.guham.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
