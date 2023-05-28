7c4520c4171288fd798ffbf207da4ae21bbca8ab
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-05-28 14:41:52 +0900

    Feat: 단방향 연관관계 의존성 테스트, 순환관계 체크 테스트

---
+++ src/test/java/com/guham/guham/PackageDependencyTests.java
@@ -0,0 +1,54 @@
+package com.guham.guham;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.dependencies.syntax.GivenSlices;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+public class PackageDependencyTests {
+
+    private static final String TEAM = "..modules.team..";
+    private static final String EVENT = "..modules.event..";
+    private static final String ACCOUNT = "..modules.account..";
+    private static final String TAG = "..modules.tag..";
+    private static final String ZONE = "..modules.zone..";
+
+    JavaClasses jc = new ClassFileImporter()
+            .importPackages("com.guham.guham..");
+
+    @Test
+    public void teamPackageRuleCheck(){
+        ArchRule teamPackageRule = classes().that().resideInAPackage(TEAM)
+                .should().accessClassesThat()
+                .resideInAnyPackage(TEAM, EVENT);
+        teamPackageRule.check(jc);
+    }
+
+    @Test
+    public void eventPackageRuleCheck(){
+        ArchRule eventPackageRule = classes().that().resideInAPackage(EVENT)
+                .should().onlyBeAccessed().byClassesThat()
+                .resideInAnyPackage(TEAM,ACCOUNT, EVENT);
+        eventPackageRule.check(jc);
+    }
+
+    @Test
+    public void accountPackageRuleCheck(){
+        ArchRule accountPackageRule = classes().that().resideInAPackage(ACCOUNT)
+                .should().accessClassesThat()
+                .resideInAnyPackage(ACCOUNT, ZONE, TAG);
+        accountPackageRule.check(jc);
+    }
+
+    @Test
+    public void test(){
+        ArchRule matching = slices().matching("com.guham.guham.modules.(*)..")
+                .should().beFreeOfCycles();
+        matching.check(jc);
+    }
+
+}
